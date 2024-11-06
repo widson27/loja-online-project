5ece2a0a11fe6494a391b4d3bd34565c38100a60
widson <widsonleite69@gmail.com>
2024-11-06 10:40:31 -0300

    feat: insert antd and styled-components

--- vendas-online-web/src/App.tsx
+++ vendas-online-web/src/App.tsx
@@ -1,7 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+
+import { Button } from 'antd';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import viteLogo from '/vite.svg';
+
+import reactLogo from './assets/react.svg';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,23 +14,34 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      <StyledDiv isBlue={count > 4} className="card">
+        <Button type="primary" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
+      </StyledDiv>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
+
+interface PStyledLink {
+  isBlue?: boolean;
+}
+
+const StyledDiv = styled.div<PStyledLink>`
+  background-color: ${(props) => (props.isBlue ? 'blue' : 'pink')};
+  border-radius: 24px;
+`;
 
 export default App;
