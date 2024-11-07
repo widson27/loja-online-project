49cbb5fdf25f86ac5c3f82fa1c2d847ba568dc49
widson <widsonleite69@gmail.com>
2024-11-07 14:44:42 -0300

    feat: creat module login

--- vendas-online-web/src/App.tsx
+++ vendas-online-web/src/App.tsx
@@ -1,47 +1,11 @@
-import './App.css';
-
-import { Button } from 'antd';
-import { useState } from 'react';
-import styled from 'styled-components';
-
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
+import LoginScreen from './modules/login';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <StyledDiv isBlue={count > 4} className="card">
-        <Button type="primary" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </StyledDiv>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <LoginScreen />
     </>
   );
 }
-
-interface PStyledLink {
-  isBlue?: boolean;
-}
-
-const StyledDiv = styled.div<PStyledLink>`
-  background-color: ${(props) => (props.isBlue ? 'blue' : 'pink')};
-  border-radius: 24px;
-`;
 
 export default App;
---
+++ vendas-online-web/src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import { BackgoundImage } from '../styles/LoginScreen.styles';
+
+const LoginScreen = () => {
+  return (
+    <div>
+      <BackgoundImage src="./background.png" />
+    </div>
+  );
+};
+
+export default LoginScreen;
